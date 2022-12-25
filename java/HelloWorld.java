7fb6a4deb70c4a48638cd9d66f6a7c8759bc894d
Константин Щуров <shurovkp@gmail.com>
2022-12-25 13:16:41 +0300

    change hello world from branch master

--- java/HelloWorld.java
+++ java/HelloWorld.java
@@ -3,5 +3,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     //21321321
+
+        //Good morning from branch master
     }
 }
